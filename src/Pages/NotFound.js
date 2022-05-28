4bc34e8385fca429d4532bce539fc401a4bb10d0
rasedul99 <rasedul2199@gmail.com>
2022-05-28 10:03:31 +0600

    notfound text

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
       <div className="mt-5 grid grid-cols-1  lg:grid-cols-2 gap-5">
         <div className="flex justify-center items-center">
           <div>
-            <h2 className="text-primary text-center">OOPS...</h2>
+            <h2 className="text-primary text-center">OOPS...!</h2>
             <h2 className="">Sorry page is not founding</h2>
           </div>
         </div>
